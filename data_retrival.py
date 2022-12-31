5b8f8c70394028b4c7dfe6d921b6db4c161fee88
hieule <hieult1996@gmail.com>
2022-12-31 14:20:11 -0600

    get filename corresponding to the specified time range

--- data_retrival.py
+++ data_retrival.py
@@ -11,6 +11,21 @@ def get_data(top_rights, bottom_lefts, times, output_path):
     print("Getting data...")
     pass
 
+def get_desired_filenames(filenames, times):
+    desired_filenames = []
+    for filename in filenames:
+        if filename.find("mask") != -1:
+            mask_filenames = filename
+            continue
+        file_time_idx = filename.rpartition(".")[0].split("_")[-1]
+        file_time_idx = int(file_time_idx)
+        if file_time_idx >= times[0] and file_time_idx <= times[1]:
+            desired_filenames.append(filename)
+            
+    desired_filenames.append(mask_filenames)
+    return desired_filenames
+
+
 if __name__ == "__main__":
     get_data()
     
